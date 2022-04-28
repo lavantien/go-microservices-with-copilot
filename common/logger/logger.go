85f35d538c9c219e8214eda03d8899d58e9a178a
lavantien <lavantien96@gmail.com>
2022-04-29 00:04:17 +0700

    Improve Logger

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -3,26 +3,33 @@ package logger
 import "log"
 
 type Logger struct {
-	logger *log.Logger
+	logger      *log.Logger
+	serviceName string
 }
 
-func NewLogger() *Logger {
+func NewLogger(serviceName string) *Logger {
 	logger := log.Default()
 	logger.SetFlags(log.LstdFlags | log.Lshortfile)
-	return &Logger{logger: logger}
+	return &Logger{
+		logger:      logger,
+		serviceName: serviceName,
+	}
 }
 
-func (l *Logger) Info(args ...interface{}) {
-	args = append([]interface{}{"[INFO]"}, args...)
+func (l *Logger) Info(args ...any) {
+	l.logger.SetPrefix("[INFO] ")
+	args = append([]any{l.serviceName}, args...)
 	l.logger.Println(args...)
 }
 
-func (l *Logger) Error(args ...interface{}) {
-	args = append([]interface{}{"[ERROR]"}, args...)
+func (l *Logger) Error(args ...any) {
+	l.logger.SetPrefix("[ERROR] ")
+	args = append([]any{l.serviceName}, args...)
 	l.logger.Println(args...)
 }
 
-func (l *Logger) Debug(args ...interface{}) {
-	args = append([]interface{}{"[DEBUG]"}, args...)
+func (l *Logger) Debug(args ...any) {
+	l.logger.SetPrefix("[DEBUG] ")
+	args = append([]any{l.serviceName}, args...)
 	l.logger.Println(args...)
 }
