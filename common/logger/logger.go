55d0ae45408c538a114b13cdd74102907d4c48a9
lavantien <lavantien96@gmail.com>
2022-04-28 23:37:33 +0700

    Implement Logger

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -10,3 +10,18 @@ func NewLogger(logger *log.Logger) *Logger {
 	logger.SetFlags(log.LstdFlags | log.Lshortfile)
 	return &Logger{logger: logger}
 }
+
+func (l *Logger) Info(args ...interface{}) {
+	args = append([]interface{}{"[INFO]"}, args...)
+	l.logger.Println(args...)
+}
+
+func (l *Logger) Error(args ...interface{}) {
+	args = append([]interface{}{"[ERROR]"}, args...)
+	l.logger.Println(args...)
+}
+
+func (l *Logger) Debug(args ...interface{}) {
+	args = append([]interface{}{"[DEBUG]"}, args...)
+	l.logger.Println(args...)
+}
